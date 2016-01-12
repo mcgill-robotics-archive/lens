09ae8ad6e453aea46d0769729d81be0f027f14ea
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-01-11 21:37:35 -0500

    Get next frame for lens UI.

--- backend/handlers/lensui.py
+++ backend/handlers/lensui.py
@@ -4,6 +4,9 @@
 
 from tornado.gen import coroutine
 from tornado.web import RequestHandler
+from image import ImageHandler
+from frame import NextFrameHandler
+import json
 
 __author__ = "Malcolm Watt"
 __version__ = "0.0.1"
@@ -15,6 +18,10 @@ class LensUIHandler(RequestHandler):
     @coroutine
     def get(self):
         """Renders lens UI"""
+        imageInfo = NextFrameHandler.objects.get()
+        imageId = json.loads(imageInfo)['id']
+        image = ImageHandler.objects.get(imageId)
+        print(image)
         self.render("lens.html")
 
     @coroutine
