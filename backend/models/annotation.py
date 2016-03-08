a7a57bb727e3555b21ad716bb9d99d311ed7eb31
Anass Al <mr@anassinator.com>
2016-03-08 05:52:16 -0500

    Add from_json() and dump() to Annotation model

--- backend/models/annotation.py
+++ backend/models/annotation.py
@@ -5,6 +5,10 @@
 from user import User
 from datetime import datetime
 from motorengine import Document, fields
+from tornado.gen import coroutine, Return
+
+__author__ = "Anass Al-Wohoush, Monica Ung"
+__version__ = "0.1.0"
 
 
 class Annotation(Document):
@@ -21,3 +25,29 @@ class Annotation(Document):
     data = fields.JsonField(required=True)
     author = fields.ReferenceField(reference_document_type=User)
     timestamp = fields.DateTimeField(default=datetime.utcnow())
+
+    def dump(self):
+        """Returns dictionary representation of annotation."""
+        return {
+            "author": self.author.name,
+            "timestamp": self.timestamp,
+            "data": self.data
+        }
+
+    @classmethod
+    @coroutine
+    def from_json(cls, json, author):
+        """Creates an annotation from a JSON blob and writes it to the
+        database.
+
+        Args:
+            json: JSON blob.
+
+        Returns:
+            Annotation.
+        """
+        annotation = yield Annotation.objects.create(
+            data=json,
+            author=author
+        )
+        raise Return(annotation)
