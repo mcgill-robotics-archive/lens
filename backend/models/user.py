186bfe1d44052b48795d16b2b2162bc76ef8f464
Anass Al <mr@anassinator.com>
2016-03-08 05:55:28 -0500

    Add from_username() and add_points() to User

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,6 +3,10 @@
 """User model."""
 
 from motorengine import Document, fields
+from tornado.gen import coroutine, Return
+
+__author__ = "Monica Ung, Anass Al-Wohoush"
+__version__ = "0.1.0"
 
 
 class User(Document):
@@ -16,4 +20,41 @@ class User(Document):
     __collection__ = "users"
 
     name = fields.StringField(required=True)
-    points = fields.IntField()
+    points = fields.IntField(default=0)
+
+    def dump(self):
+        """Returns dictionary representation of user."""
+        return {
+            "name": self.name,
+            "points": self.points
+        }
+
+    @classmethod
+    @coroutine
+    def from_username(cls, name):
+        """Creates a user and writes it to the database.
+
+        Args:
+            name: Username.
+
+        Returns:
+            User.
+        """
+        # Look up if user already exists.
+        users = yield User.objects.filter(name=name).limit(1).find_all()
+        if users:
+            raise Return(users[0])
+
+        # Otherwise, create a new one.
+        user = yield User.objects.create(name=name)
+        raise Return(user)
+
+    @coroutine
+    def add_points(self, pts):
+        """Adds points to the user.
+
+        Args:
+            pts: Number of points to increment by.
+        """
+        self.points += pts
+        yield self.save()
