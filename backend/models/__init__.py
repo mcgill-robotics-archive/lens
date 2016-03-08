621a047e59b6bdbb090869692456de5dc9dae692
Anass Al <mr@anassinator.com>
2016-03-08 05:55:53 -0500

    Fix models/__init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,8 +2,12 @@
 
 """Lens Backend Models."""
 
+from tag import Tag
+from bag import Bag
+from feed import Feed
+from user import User
 from frame import Frame
-from video import Video
+from annotation import Annotation
 
 __author__ = "Anass Al-Wohoush"
 __version__ = "0.3.0"
