64a090969af9e8e981fbc1f9c3464f651431ae74
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-04-04 21:36:54 -0400

    Fix Firefox clientWidth & clientHeight issue

--- backend/views/annotation_tool/javascript/Frame.js
+++ backend/views/annotation_tool/javascript/Frame.js
@@ -51,16 +51,18 @@ Frame.prototype.fitToPage = function() {
   image.style.height = pageHeight + "px";
 
   // Now we need to check the aspect ratio and adjust accordingly
-  var aspectRatio = image.clientWidth / image.clientHeight;
+  var imageWidth = image.clientWidth || image.parentElement.clientWidth;
+  var imageHeight = image.clientHeight || image.parentElement.clientHeight;
+  var aspectRatio = imageWidth / imageHeight;
 
   if (aspectRatio > Lens.image.aspectRatio) {
     // Limiting factor is height
-    var adjustedWidth = image.clientHeight * Lens.image.aspectRatio;
-    var adjustedWidthPercentage = adjustedWidth / image.clientWidth;
+    var adjustedWidth = imageHeight * Lens.image.aspectRatio;
+    var adjustedWidthPercentage = adjustedWidth / imageWidth;
     image.style.width = adjustedWidthPercentage * 100 + '%';
   } else {
     // Limiting factor is width
-    var adjustedHeight = image.clientWidth / Lens.image.aspectRatio;
+    var adjustedHeight = imageWidth / Lens.image.aspectRatio;
     image.style.height = adjustedHeight + 'px';
   }
 };
