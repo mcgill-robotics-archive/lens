006764e69dc8e1b3236f62474b3fd76550f6c043
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-06-21 19:42:06 -0400

    Remove unused methods from lensui.py

--- backend/handlers/lensui.py
+++ backend/handlers/lensui.py
@@ -7,30 +7,13 @@ from tornado.web import RequestHandler
 import json
 
 __author__ = "Malcolm Watt"
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 
 class LensUIHandler(RequestHandler):
-    @coroutine
-    def prepare(self):
-        """ Parse JSON request bodies to allow access to form data.
-        """
-        is_json_req = self.request.headers["Content-Type"].
-            startswith("application/json")
-
-        if self.request.method == "POST" and is_json_req:
-            self.json_args = json.loads(self.request.body)
-        else
-            self.json_args = None
-
-
+    
     @coroutine
     def get(self):
         """ Handles annotation tool application template dispatch
         """
         self.render('annotate.html')
 
-    @coroutine
-    def post(self):
-        """ Writes the metadata for the annotated frame to the database.
-        """
-        pass
